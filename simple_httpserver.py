88d33d5f75cc97cb9e973b593923e58615036deb
Samarth Brahmbhatt <samarth.robo@gmail.com>
2022-07-06 21:59:50 -0700

    local debug server upgrade to python3

--- simple_httpserver.py
+++ simple_httpserver.py
@@ -1,11 +1,9 @@
-import SimpleHTTPServer
-import SocketServer
+from http.server import SimpleHTTPRequestHandler
+from socketserver import TCPServer
 
 PORT = 8000
 
-Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-
-httpd = SocketServer.TCPServer(("", PORT), Handler)
+httpd = TCPServer(("", PORT), SimpleHTTPRequestHandler)
 
 print("serving at port {:d}".format(PORT))
 httpd.serve_forever()
